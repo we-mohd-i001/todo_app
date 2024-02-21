5906a2f68179732bb4a3917db7f628f244cf75f0
Mohd Imran <mohd-i001@webreinvent.com>
2024-02-21 12:45:58 +0530

    Added route
    - routes
    - navigator key
    - two roots 1. settings, 2. start (dummy)
    - route observer (NavigatorObserver)

--- lib/application/app/basic_app.dart
+++ lib/application/app/basic_app.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:todo_app/application/core/routes.dart';
 
 class BasicApp extends StatelessWidget {
   const BasicApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp.router(
+      title: 'Todo App',
+      localizationsDelegates: const [
+        ...GlobalMaterialLocalizations.delegates,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      themeMode: ThemeMode.system,
+      theme: ThemeData.from(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      ),
+      darkTheme: ThemeData.from(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      ),
+      routerConfig: routes,
+    );
   }
 }
