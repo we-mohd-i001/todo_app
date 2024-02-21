cfe06867feb81892958a2c349cd3a6e57127c0f3
Mohd Imran <mohd-i001@webreinvent.com>
2024-02-21 10:58:29 +0530

    Initial

--- lib/application/app/basic_app.dart
+++ lib/application/app/basic_app.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:localization/localization.dart';
 class BasicApp extends StatelessWidget {
   const BasicApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp.router(
+      title: 'Todo App',
+      localizationsDelegates: [
+      GlobalMaterialLocalizations.delegate,
+
+      ],
+    );
   }
 }
