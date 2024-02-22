8a22fe11c61374e119497faf252ad3946e3c59ba
Mohd Imran <mohd-i001@webreinvent.com>
2024-02-22 19:00:39 +0530

    fix added const keyword

--- lib/application/components/view_states/todo_entry_item.dart
+++ lib/application/components/view_states/todo_entry_item.dart
@@ -4,7 +4,6 @@ import 'package:todo_app/application/components/bloc/todo_entry_item_cubit.dart'
 import 'package:todo_app/application/components/view_states/todo_entry_item_error.dart';
 import 'package:todo_app/application/components/view_states/todo_entry_item_loaded.dart';
 import 'package:todo_app/application/components/view_states/todo_entry_item_loading.dart';
-import 'package:todo_app/application/pages/detail/bloc/todo_detail_cubit.dart';
 import 'package:todo_app/domain/entities/unique_id.dart';
 import 'package:todo_app/domain/repositories/todo_repository.dart';
 import 'package:todo_app/domain/use_cases/load_todo_entry.dart';
@@ -38,7 +37,7 @@ class TodoEntryItem extends StatelessWidget {
     return BlocBuilder<TodoEntryItemCubit, TodoEntryItemState>(
         builder: (context, state) {
           if(state is TodoEntryItemStateLoading){
-            return TodoEntryItemLoading();
+            return const TodoEntryItemLoading();
           }
           else if(state is TodoEntryItemStateLoaded){
             return TodoEntryItemLoaded(
@@ -46,7 +45,7 @@ class TodoEntryItem extends StatelessWidget {
             );
           }
           else{
-            return TodoEntryItemError();
+            return const TodoEntryItemError();
           }
         });
   }
