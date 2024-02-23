6b78b54aa02c1daf7e188bc4a780eb2a1043ef89
Mohd Imran <mohd-i001@webreinvent.com>
2024-02-23 16:04:04 +0530

    added UI for adding the collections

--- lib/application/pages/create_todo_collection/create_todo_collection_page.dart
+++ lib/application/pages/create_todo_collection/create_todo_collection_page.dart
@@ -1,14 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:todo_app/application/core/page_config.dart';
+import 'package:todo_app/domain/entities/todo_color.dart';
 
-class CreateTodoCollectionPage extends StatelessWidget {
-  const CreateTodoCollectionPage({super.key});
-
-  static const pageConfig = PageConfig(icon: Icons.add_task_rounded , name: 'create_todo_collection');
-  
+class CreateTodoCollectionPageProvider extends StatelessWidget {
+  const CreateTodoCollectionPageProvider({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const CreateTodoCollectionPage();
+  }
+}
+
+class CreateTodoCollectionPage extends StatefulWidget {
+  const CreateTodoCollectionPage({super.key});
+
+  static const pageConfig = PageConfig(
+    icon: Icons.add_task_rounded,
+    name: 'create_todo_collection',
+    child: CreateTodoCollectionPage(),
+  );
+
+  @override
+  State<CreateTodoCollectionPage> createState() =>
+      _CreateTodoCollectionPageState();
+}
+
+class _CreateTodoCollectionPageState extends State<CreateTodoCollectionPage> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a title!';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Color'),
+              validator: (value) {
+                if (value != null && value.isNotEmpty) {
+                  final parseColorIndex = int.tryParse(value);
+                  if (parseColorIndex == null ||
+                      parseColorIndex < 0 ||
+                      parseColorIndex > TodoColor.predefinedColors.length) {
+                    return 'Only numbers between and ${TodoColor.predefinedColors.length - 1} are allowed';
+                  }
+                }
+                return null;
+              },
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final isValid = _formKey.currentState?.validate();
+                if (isValid == true) {
+                  context.pop();
+                }
+              },
+              child: const Text('Save Collection'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
